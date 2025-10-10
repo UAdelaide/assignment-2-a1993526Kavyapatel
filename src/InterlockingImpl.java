ea0d8ff2ed0517808b50b4538fcb2e9d4fceee28
a1993526Kavyapatel <a1993526@student.adelaide.edu.au>
2025-10-10 19:01:31 +1030

    Solving moveTrains()Methods errors for Interlockingimpl.java  autograder tests

--- src/InterlockingImpl.java
+++ src/InterlockingImpl.java
@@ -1,17 +1,5 @@
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Implements the Interlocking interface for Programming Assignment 2.
